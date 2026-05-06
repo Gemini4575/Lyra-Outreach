9f96c4c080aad202da79c8026eec1f6cfea3ce20
shhsrobotics@gmail.com <1.tyler.j.sullivan@gmail.com>
2026-05-05 20:20:11 -0400

    Add shuffleboard shooter speed control

--- Lyra Outreach/Lyra Outreach/src/main/java/frc/robot/Shoot.java
+++ Lyra Outreach/Lyra Outreach/src/main/java/frc/robot/Shoot.java
@@ -1,22 +1,31 @@
 package frc.robot;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.networktables.GenericEntry;
+
 public class Shoot {
     private Spark m_shooter;
-    Random_Number m_random;
-
+    private GenericEntry m_shooterSpeed;
+    private GenericEntry m_shooterSpeedReverse;
 
     public Shoot (int shooter) {
         m_shooter = new Spark(shooter);
-        m_random = new Random_Number();
+        m_shooterSpeed = Shuffleboard.getTab("Main")
+            .add("Shooter Speed", 1.0)
+            .getEntry();
+        
+        m_shooterSpeedReverse = Shuffleboard.getTab("Main")
+            .add("Shooter Speed Reverse", -1.0)
+            .getEntry();
     }
 
     public void Shooot (boolean Bbuuton, boolean XBuuton, boolean Ybuuton) {
          if (Bbuuton) {
-            m_shooter.set(-1);
+            m_shooter.set(m_shooterSpeedReverse.getDouble(-1.0));
         } else if (XBuuton) {
-            m_shooter.set(Math.random() * -1);
+            
         } else if (Ybuuton) {
-            m_shooter.set(1);
+            m_shooter.set(m_shooterSpeed.getDouble(1.0));
         } else {
             m_shooter.set(0);
         }
